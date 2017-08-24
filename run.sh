72cc9930dbbec477ce2f17e23dc6aca10d60424d
Tao Wang <twang2218@gmail.com>
2017-08-24 19:39:27 +1000

    Adjust build script for Travis CI
    Signed-off-by: Tao Wang <twang2218@gmail.com>

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-IMAGE_NAME=twang2218/spark
-IMAGE_TAG=test
+DOCKER_USERNAME=${DOCKER_USERNAME:-twang2218}
+IMAGE_NAME=${IMAGE_NAME:-${DOCKER_USERNAME}/spark}
+IMAGE_TAG=${IMAGE_TAG:-latest}
 
 SPARK_VERSION=2.2.0
 HADOOP_VERSION=2.7
+
+VARIATION="generic scala R python"
 
 function generate_dockerfile() {
   local dep=$1
@@ -22,7 +25,6 @@ function generate_dockerfile() {
     > $file
 }
 
-
 function generate() {
   generate_dockerfile "" $SPARK_VERSION $HADOOP_VERSION spark-shell scala/Dockerfile
   generate_dockerfile "R" $SPARK_VERSION $HADOOP_VERSION sparkR R/Dockerfile
@@ -32,7 +34,7 @@ function generate() {
 
 function build() {
   generate
-  for t in scala R python generic; do
+  for t in $VARIATION; do
     docker build -t ${IMAGE_NAME}:${t} ${t}
   done
 }
@@ -40,51 +42,59 @@ function build() {
 function run() {
   local version=${1:-$IMAGE_TAG}
   shift
-  docker run -it -p 7077:7077 -p 8080:8080 ${IMAGE_NAME}:${version} "$@"
-}
-
-function service_run() {
-  local version=${1:-$IMAGE_TAG}
-  shift
-  docker network create spark
-  docker run -d \
-    -p 7077:7077 \
-    -p 8080:8080 \
-    --network spark \
-    --name spark-master \
-    ${IMAGE_NAME}:${version} \
-    start-master.sh \
-      --host spark-master
-
-  docker run -d \
-    -p 8081:8081 \
-    --network spark \
-    --name spark-worker \
-    ${IMAGE_NAME}:${version} \
-    start-slave.sh \
-      --host spark-worker \
-      spark://spark-master:7077
-}
-
-function service_clean() {
-  docker container rm -f spark-worker spark-master
-  docker network rm spark
-}
-
-function service() {
-  local command=$1
-  shift
-  case $command in
-    run)    service_run scala ;;
-    clean)  service_clean ;;
-    *)      echo "Usage: $0 service (run|clean)" ;;
-  esac
+  docker run -it --rm -p 7077:7077 -p 8080:8080 ${IMAGE_NAME}:${version} "$@"
 }
 
 function release() {
-  for tag in scala R python generic; do
+  for tag in $VARIATION; do
     docker push ${IMAGE_NAME}:${tag}
   done
+}
+
+function trigger_build() {
+  local tag=$1
+  if [ -n "$DOCKER_TRIGGER_TOKEN" ]; then
+    curl --silent \
+      --header "Content-Type: application/json" \
+      --request POST \
+      --data "{\"docker_tag\": \"$tag\"}" \
+      https://registry.hub.docker.com/u/${IMAGE_NAME}/trigger/${DOCKER_TRIGGER_TOKEN}
+    echo -e "\ndone."
+  else
+    echo -e "\nDOCKER_TRIGGER_TOKEN is empty"
+  fi
+}
+
+function ci() {
+  if [[ -n "${DOCKER_PASSWORD}" ]]; then
+    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+  else
+    echo "Cannot login to Docker Hub (DOCKER_PASSWORD is empty)"
+    return 1
+  fi
+
+  # We just trigger the `base` build to refresh the README on the Hub
+  trigger_build scala
+
+  if [ "$1" == "--force" ]; then
+    # build all no matter it's changed or not
+    build
+    release
+  else
+    for tag in $VARIATION; do
+      if (git show --pretty="" --name-only | grep Dockerfile | grep -q $tag); then
+        echo "$tag has been updated, rebuilding ${IMAGE_NAME}:$tag ..."
+        docker build -t ${IMAGE_NAME}:${tag} ${tag}
+        echo "Publish image '${IMAGE_NAME}:${tag}' to Docker Hub ..."
+        docker push ${IMAGE_NAME}:${tag}
+      else
+        echo "Nothing changed in $tag."
+      fi
+    done
+  fi
+
+  # List all the images
+  docker images
 }
 
 function main() {
@@ -94,9 +104,9 @@ function main() {
     generate) generate "$@" ;;
     build)    build "$@" ;;
     run)      run "$@" ;;
-    service)  service "$@" ;;
     release)  release "$@" ;;
-    *)        echo "Usage: $0 (generate|build|run|service|release)" ;;
+    ci)       ci "$@" ;;
+    *)        echo "Usage: $0 (generate|build|run|release|ci)" ;;
   esac
 }
 
